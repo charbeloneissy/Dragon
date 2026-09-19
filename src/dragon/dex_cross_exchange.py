3da736be842066ee981817bc8da9eff724062cbe
coneissy-ctrl <coneissy@gmail.com>
2026-09-19 11:54:21 +0300

    Fix fast scanner runtime import (#11)

--- src/dragon/dex_cross_exchange.py
+++ src/dragon/dex_cross_exchange.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import os
+import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import dataclass
 from decimal import Decimal
